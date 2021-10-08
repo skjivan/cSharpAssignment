8b5b6f660c1928532bd6742fca9e162bf76f1aab
skjivan <shalujivan@gmail.com>
2021-10-08 12:48:00 -0500

    This is the final

--- cSharpAssignment/Program.cs
+++ cSharpAssignment/Program.cs
@@ -16,256 +16,189 @@ namespace cSharpAssignment
         {
             Console.WriteLine("Hello World");
 
-            insertTopCharts();
-            //insertNumStreams();
-            //insertDaysOnRadio();
             //insertSpotifyStreams();
-            //insertAppleStreams();
-
+            //insertTopCharts();
+            insertDaysOnRadio();
         }
 
         public static void insertTopCharts()
         {
+            //variables
+            double calculation = 0;
+            int count = 1;
+            double avgWOCPercent;
+            double averageWOC;
+
             IWebDriver driver = new ChromeDriver("S:/skjivan/bdp");         //this is where the chromedriver is
-            string connectionString = ("Data Source=essql1.walton.uark.edu;Initial Catalog=PROJECTF2111;User ID=PROJECTF2111;Password=EK85gon$");
-            //SqlConnection conn = new SqlConnection(@"Data Source=essql1.walton.uark.edu;Initial Catalog=PROJECTF2111;User ID=PROJECTF2111;Password=EK85gon$");
-            SqlConnection connection;
-            SqlCommand command;
-            SqlDataReader dataReader;
-
-            string[] chartsArray = new string[1500];
-            int counter = 0;
-
             driver.Url = "https://www.officialcharts.com/charts/singles-chart/table";             //top charts
-            IWebElement topChartTable = driver.FindElement(By.ClassName("chart-positions"));      //top charts               //this one works with the className instead of id
-
+            IWebElement topChartTable = driver.FindElement(By.XPath("/html/body/div[3]/div[1]/article/div/div[1]/section/table"));      //top charts               
             var rows = topChartTable.FindElements(By.TagName("tr"));
-            foreach (var row in rows)
-            {
-                var tds = row.FindElements(By.TagName("td"));
-                foreach (var entry in tds)
-                {
-                    chartsArray[counter] = entry.Text;
-                    Console.WriteLine(counter + "this is a row " + chartsArray[counter]);
-                    counter++;
-                }
-                Console.WriteLine();
-            }
 
-            connection = new SqlConnection(connectionString);
-            connection.Open();
-            string sql = "INSERT INTO topCharts VALUES (@pos, @lw, @title, @album, @artist, @peakPos, @woc, @buy)"; //scaler variables dont have to have the same names as sql tables but they have to be in order
-            command = new SqlCommand(sql, connection);
+            Console.WriteLine();
+            Console.WriteLine("Calculating average weeks on chart...");
+            Console.WriteLine("Calculating top 10 songs that have been on the charts the longest");
+            Console.WriteLine();
 
-            command.Parameters.AddWithValue("@pos", chartsArray[0]);
-            command.Parameters.AddWithValue("@lw", chartsArray[1]);
-            command.Parameters.AddWithValue("@title", chartsArray[2].ToString());
-            command.Parameters.AddWithValue("@album", chartsArray[3].ToString());
-            command.Parameters.AddWithValue("@artist", chartsArray[4].ToString());
-            command.Parameters.AddWithValue("@peakPos", chartsArray[5]);
-            command.Parameters.AddWithValue("@woc", chartsArray[6]);
-            command.Parameters.AddWithValue("@buy", chartsArray[7].ToString());
-
-            command.ExecuteNonQuery();
-
-            connection.Close();
-            command.Dispose();
-
-            driver.Quit();
-            Console.Write("press any key to end ...");
-            Console.ReadLine();
-        }
-
-        public static void insertNumStreams()
-        {
-            IWebDriver driver = new ChromeDriver("S:/skjivan/bdp");         //this is where the chromedriver is
-            SqlConnection conn = new SqlConnection(@"Data Source=essql1.walton.uark.edu;Initial Catalog=PROJECTF2111;User ID=PROJECTF2111;Password=EK85gon$");
-            string[] streamsArray = new string[1500];
-            int counter = 0;
-
-            driver.Url = "https://hitsdailydouble.com/streaming_songs";                                     //number of streams
-            IWebElement totalStreamsTable = driver.FindElement(By.ClassName("hits_album_chart"));           //number of streams         //this one works with the className
-
-
-                var rows = totalStreamsTable.FindElements(By.TagName("tr"));
+            //the actual calculation
                 foreach (var row in rows)
                 {
-                    var tds = row.FindElements(By.TagName("td"));
+                    var tds = row.FindElements(By.XPath("td[5]"));
                     foreach (var entry in tds)
                     {
-                        streamsArray[counter] = entry.Text;
-                        Console.WriteLine(counter + "this is the row " + streamsArray[counter]);
-                        counter++;
+                    double WOC;
+                    string temp = entry.Text;
+                    WOC = Convert.ToDouble(temp);
+
+                    calculation += WOC;
+                    averageWOC = (calculation / 100);
+
+                    avgWOCPercent = (WOC / calculation) * 100;
+                    avgWOCPercent = Math.Round(avgWOCPercent, 2);
+
+                    if ((WOC > averageWOC) && (count < 11))
+                    {
+                        Console.WriteLine(count + ". Number of Weeks on Charts: " + WOC + Environment.NewLine + "   WOC Percentage: " + avgWOCPercent + "%");
                     }
 
-                    Console.WriteLine();
+                    var song = row.FindElements(By.XPath("td[3]"));
+                    foreach (var entry2 in song)
+                    {
+                        string songName = entry2.Text;
+                        if ((WOC > averageWOC) && (count < 11))
+                        {
+                            Console.WriteLine(count + ". Name of Song: " + songName);
+                            Console.WriteLine();
+                            count++;
+                        }
+                    }
+                }
                 }
 
-            driver.Quit();
-            Console.Write("press any key to end ...");
-            Console.ReadLine();
+                driver.Quit();
+                Console.Write("press any key to end ...");
+                Console.ReadLine();
         }
 
         public static void insertDaysOnRadio()
         {
+            //variables
+            double calculation = 0;
+            int count = 1;
+            double avgDaysPercent;
+            double avgDays;
+
+            //change the website and xpath here for each different website
             IWebDriver driver = new ChromeDriver("S:/skjivan/bdp");         //this is where the chromedriver is
-            //SqlConnection conn = new SqlConnection(@"Data Source=essql1.walton.uark.edu;Initial Catalog=PROJECTF2111;User ID=PROJECTF2111;Password=EK85gon$");
-            string[] radioArray = new string[1500];
-            int counter = 0;
-            string connectionString = ("Data Source=essql1.walton.uark.edu;Initial Catalog=PROJECTF2111;User ID=PROJECTF2111;Password=EK85gon$");
-            SqlConnection connection;
-            SqlCommand command;
-            SqlDataReader dataReader;
-            connection = new SqlConnection(connectionString);
-            connection.Open();
-            string sql = "INSERT INTO radioStreams VALUES(@position, @pos, @title, @days, @pk, @aud, @audpt2, @formats, @pkaud, @itunes, @spotify, @apple, @shazam)";
-            command = new SqlCommand(sql, connection);
-
-            //command.Parameters.AddWithValue("@position", );
-            //command.Parameters.AddWithValue();
-            //command.Parameters.AddWithValue();
-
             driver.Url = "https://kworb.net/radio/";                                              //days on the radio     //THIS ONE WORKS
-            IWebElement daysOnRadioTable = driver.FindElement(By.Id("overallai"));                //days on the radio     //THIS ONE WORKS with the normal
-
+            IWebElement daysOnRadioTable = driver.FindElement(By.XPath("/html/body/div[2]/div[4]/table"));                //days on the radio     //THIS ONE WORKS with the normal
             var rows = daysOnRadioTable.FindElements(By.TagName("tr"));
+
+            Console.WriteLine();
+            Console.WriteLine("Calculating the average number of days on the radio...");
+            Console.WriteLine("Calculating the top 10 songs on the radio the longest...");
+            Console.WriteLine();
+
+            //actual calculation
             foreach (var row in rows)
             {
-                var tds = row.FindElements(By.TagName("td"));
+                var tds = row.FindElements(By.XPath("td[4]"));
                 foreach (var entry in tds)
                 {
-                    radioArray[counter] = entry.Text;
-                    //Console.WriteLine(counter + "this is the row" + radioArray[counter]);
-                    Console.WriteLine(radioArray[counter]);
+                    double days;
+                    string temp = entry.Text;
+                    days = Convert.ToDouble(temp);
+
+                    calculation += days;
+                    avgDays = (calculation / 100);
+
+                    avgDaysPercent = (days / calculation) * 100;
+                    avgDaysPercent = Math.Round(avgDaysPercent, 2);
+
+                    if ((days > avgDays) && (count < 11))
+                    {
+                        Console.WriteLine(count + ". Number of Days: " + days + Environment.NewLine + "   Days Percentage: " + avgDaysPercent + "%");
+                    }
+
+                    var song = row.FindElements(By.XPath("td[3]"));
+                    foreach (var entry2 in song)
+                    {
+                        string songName = entry2.Text;
+                        if ((days > avgDays) && (count < 11))
+                        {
+                            Console.WriteLine(count + ". Name of Song: " + songName);
+                            Console.WriteLine();
+                            count++;
+                        }
+                    }
                 }
-                Console.WriteLine();
             }
 
-            command.Parameters.AddWithValue("@position", radioArray[0]);
-            command.Parameters.AddWithValue("@pos", radioArray[1]);
-            command.Parameters.AddWithValue("@title", radioArray[2].ToString());
-            command.Parameters.AddWithValue("@days", radioArray[3]);
-            command.Parameters.AddWithValue("@pk", radioArray[4]);
-            command.Parameters.AddWithValue("@aud", radioArray[5]);
-            command.Parameters.AddWithValue("@audpt2", radioArray[6]);
-            command.Parameters.AddWithValue("@formats", radioArray[7]);
-            command.Parameters.AddWithValue("@pkaud", radioArray[8]);
-            command.Parameters.AddWithValue("@itunes", radioArray[9]);
-            command.Parameters.AddWithValue("@spotify", radioArray[10]);
-            command.Parameters.AddWithValue("@apple", radioArray[11]);
-            command.Parameters.AddWithValue("@shazam", radioArray[12]);
-
-            command.ExecuteNonQuery();
-            connection.Close();
-            command.Dispose();
+            //ending everything and closing it all out
             driver.Quit();
-
             Console.Write("press any key to end ...");
             Console.ReadLine();
         }
 
         public static void insertSpotifyStreams()
         {
-            IWebDriver driver = new ChromeDriver("S:/skjivan/bdp");         //this is where the chromedriver is
-            //string connectionString = ("Data Source=essql1.walton.uark.edu;Initial Catalog=PROJECTF2111;User ID=PROJECTF2111;Password=EK85gon$");
-            //SqlConnection connection;
-            //SqlCommand command;
-            //SqlDataReader dataReader;
-            string[] spotifyArray = new string[1500];
-            int counter = 0;
+            //variables
+            double calculation = 0;
+            int count = 1;
+            double avgPercent;
+            double averageStreams;
 
+            IWebDriver driver = new ChromeDriver("S:/skjivan/bdp");         //this is where the chromedriver is
             driver.Url = "https://spotifycharts.com/regional";                                      //spotify streams
-            IWebElement spotifyStreamsTable = driver.FindElement(By.ClassName("chart-table"));      //spotify streams       //this one works with the className
+            IWebElement spotifyStreamsTable = driver.FindElement(By.XPath("/html/body/div/div/div/div/span/table"));      //spotify streams       //this one works with the className
+            var rows = spotifyStreamsTable.FindElements(By.TagName("tr"));
 
-             var rows = spotifyStreamsTable.FindElements(By.TagName("tr"));
-             foreach (var row in rows)
-             {
-                var tds = row.FindElements(By.TagName("td"));
-                foreach (var entry in tds)
-                {
-                    spotifyArray[counter] = entry.Text;
-                    Console.WriteLine(counter + "this is an attempt" + spotifyArray[counter]);
-                    counter++;
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine();
+            Console.WriteLine("Calculating the average number of streams... \n");
+            Console.WriteLine("Calculating the percentage of the total streams the top 10 streaming songs are...");
+            Console.WriteLine();
 
-
-
-            //connection = new SqlConnection(connectionString);
-            //connection.Open();
-            //string sql = "INSERT INTO spofityStreams VALUES (@position, @title, @streams)";
-            //command = new SqlCommand(sql, connection);
-
-            //command.Parameters.AddWithValue("@position", );
-            //command.Parameters.AddWithValue();
-            //command.Parameters.AddWithValue();
-
-            //command.ExecuteNonQuery();
-            //connection.Close();
-            //command.Dispose();
-
-            //INSERT INTO [dbo].[spofityStreams] ([position], [title], [streams]) VALUES (< position, int,>, < title, varchar(100),>, < streams, int,>)
-
-            //private void btnInsertQuest_Click(object sender, EventArgs e)
-            //{
-            //    try
-            //    {
-            //        int answer;
-            //        connection = new SqlConnection(connectionString);
-            //        connection.Open();
-            //        string sql = "INSERT INTO quests VALUES (@NPC, @req, @loot)"; //scaler variables dont have to have the same names as sql tables but they have to be in order
-            //        command = new SqlCommand(sql, connection);
-
-            //        command.Parameters.AddWithValue("@NPC", txtNPC.Text);
-            //        command.Parameters.AddWithValue("@req", txtReqs.Text);
-            //        command.Parameters.AddWithValue("@loot", txtLoot.Text);
-
-            //        answer = command.ExecuteNonQuery();
-
-            //        connection.Close();
-            //        command.Dispose();
-
-            //        MessageBox.Show("You successfully inserted " + answer + " quest record!!");
-            //    }
-            //    catch (Exception error)
-            //    {
-            //        MessageBox.Show("Hey you did not successfully insert a quest. look at this error  " + error);
-            //    }
-            //}
-
-            driver.Quit();
-            Console.Write("press any key to end ...");
-            Console.ReadLine();
-        }
-
-        public static void insertAppleStreams()
-        {
-            IWebDriver driver = new ChromeDriver("S:/skjivan/bdp");         //this is where the chromedriver is
-            SqlConnection conn = new SqlConnection(@"Data Source=essql1.walton.uark.edu;Initial Catalog=PROJECTF2111;User ID=PROJECTF2111;Password=EK85gon$");
-            string[] appleArray = new string[1000];
-            int counter = 0;
-
-            driver.Url = "https://kworb.net/charts/apple_a/us.html";                           //appleMusic streams    //THIS ONE WORKS
-            IWebElement appleStreamTable = driver.FindElement(By.Id("simpletable"));           //appleMusic streams    //THIS ONE WORKS with the normal
-
-            var rows = appleStreamTable.FindElements(By.TagName("tr"));
             foreach (var row in rows)
             {
-                var tds = row.FindElements(By.TagName("td"));
-                foreach (var entry in tds) 
+                var tds = row.FindElements(By.XPath("td[5]"));
+                foreach(var entry in tds)
                 {
-                    appleArray[counter] = entry.Text;
-                    Console.WriteLine(counter + "this is the last try " + appleArray[counter]);
-                    counter++;
+                    double stream;
+                    string temp = entry.Text;
+                    stream = Convert.ToDouble(temp);
+
+                    calculation += stream;
+                    averageStreams = (calculation / 200);
+
+                    avgPercent = (stream / calculation) * 100;
+                    avgPercent = Math.Round(avgPercent, 2);
+
+                    if ((stream > averageStreams) && (count < 11))
+                    {
+                        Console.WriteLine(count + ". Number of Streams: " + stream + Environment.NewLine + "   Stream Percentage: " + avgPercent + "%");
+                    }
+
+                    var song = row.FindElements(By.XPath("td[4]"));
+                    foreach (var entry2 in song)
+                    {
+                        string songName = entry2.Text;
+                        if((stream > averageStreams) && (count < 11))
+                        {
+                            Console.WriteLine(count + ". Name of Song: " + songName);
+                            Console.WriteLine();
+                            count++;
+                        }
+                    }
                 }
-                Console.WriteLine();
             }
-           
+
+            Console.WriteLine();
+            Console.WriteLine("This is the average number of streams on spotify " + calculation + ".");
+
             driver.Quit();
             Console.Write("press any key to end ...");
             Console.ReadLine();
         }
+
     }
 }
 
